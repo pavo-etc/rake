026c667386c694faff0f7747c6c72379238f1ebb
demstar16 <44187160+demstar16@users.noreply.github.com>
2022-05-06 13:55:27 +0800

    read the file properly

--- rake-p/parsing.py
+++ rake-p/parsing.py
@@ -5,16 +5,18 @@ def main():
 
     if len(sys.argv) > 1:
         with open(sys.argv[1], "r") as file:
-            next(file)
             for line in file:
-                if not line.isspace():
+                line = line.partition('#')[0]
+                line = line.rstrip()
+                if line != '':
                     rakefile_lines.append(line)
     else:
-        print("No file specified, will execute default Rakefile\n") #for testing/know what's happening
+        print("\n-----No file specified, will execute default Rakefile-----\n") #for testing/know what's happening
         with open("../Rakefile", "r") as file:
-            next(file)
             for line in file:
-                if not line.isspace():
+                line = line.partition('#')[0]
+                line = line.rstrip()
+                if line != '':
                     rakefile_lines.append(line)
    
     for i in rakefile_lines:
