4f9065f7c5ad5702471e0c29aca30fb669f9024f
pavo-etc <z1verdeman2@gmail.com>
2022-05-24 18:03:11 +0800

    Complete functional version of C client

--- rake-c/rake-c.c
+++ rake-c/rake-c.c
@@ -17,6 +17,17 @@ void slice(const char *in, char *out, size_t start, size_t end) {
     strncpy(out, in + start, end - start);
 }
 
+int strtokcount(char *string, char *delim) {
+    char *token = strtok(strdup(string), delim);
+    int count = 0;
+
+    while (token != NULL) {
+        count++;
+        token = strtok(NULL, delim);
+    }
+    return count;
+}
+
 void missing_file(char *command_data, char *filename) {
     fprintf(stderr, "Error: missing file\n\tcommand: %d\n\tfilename: %s", command_data[0], filename);
     exit(EXIT_FAILURE);
@@ -38,7 +49,7 @@ void read_file(char *filename) {
         fprintf(stderr, "Error opening the file %s\n", filename);
         exit(EXIT_FAILURE);
     }
-    actionsets = calloc(1, sizeof(ACTIONSET));
+    actionsets = calloc(1, sizeof(ACTIONSET)+1);
     CHECK_ALLOC(actionsets);
 
     int port_len = 0;
@@ -93,7 +104,7 @@ void read_file(char *filename) {
                     //printf("Added port %s\n", combined);
                     // this is sus since combined could be smaller than previous.  should replace strlen(combined) with something that will always increase size
                     //could you explain this please, if my understanding is right couldn't we simply do an if statement here to make sure it is not a decrease?
-                    hosts = realloc(hosts, (nhosts+1) * sizeof(char) * strlen(combined)); 
+                    hosts = realloc(hosts, (nhosts+1) * sizeof(char) * 50); 
                     CHECK_ALLOC(hosts);
                     hosts[nhosts] = strdup(combined);
                     CHECK_ALLOC(hosts[nhosts]);
@@ -102,7 +113,7 @@ void read_file(char *filename) {
                     //printf("Has port!\n");
                     // this is sus since given_host could be smaller than previous.  should replace strlen(given_host) with something that will always increase size
                     //once again couldn't we do an if, but then i suppose we could be wasting space if we get somthing smaller
-                    hosts = realloc(hosts, (nhosts+1) * sizeof(char) * strlen(given_host));
+                    hosts = realloc(hosts, (nhosts+1) * sizeof(char) * 50);
                     CHECK_ALLOC(hosts);
                     hosts[nhosts] = strdup(given_host);
                     CHECK_ALLOC(hosts[nhosts]);
@@ -196,23 +207,20 @@ void read_file(char *filename) {
 }
 
 void send_msg(int sock, void *msg, uint32_t nbytes) {
-    // big endian nbyrtes
+    // big endian nbytes
     uint32_t be_nbytes = htonl(nbytes);
     send(sock, &be_nbytes, 4, 0);
     send(sock, msg, nbytes, 0);
 }
 
-void *recv_msg(int sock) {
-    //printf("Attempting msg len\n");
+void *recv_msg(int sock, int *size) {
     uint32_t *msg_len_ptr = calloc(1, 4);
-    recv(sock, msg_len_ptr, 4, 0);
-    //printf("Recved msg len\n");
+    int nbytes = recv(sock, msg_len_ptr, 4, 0);
+    if (nbytes == 0) return NULL;
 
     uint32_t msg_len = ntohl(*msg_len_ptr);
-    //printf("%d\n", msg_len);
-
+    *size = msg_len;
     void *msg = calloc(1, msg_len);
-
     recv(sock, msg, msg_len, 0);
 
     return msg;
@@ -236,19 +244,110 @@ int create_socket(char *hostname, char *port) {
     return s;
 }
 
-void send_command(int i, COMMAND *cmddata) {
+int find_host() {
+    int mincost = 1000;
+    int best_host_index = 0;
+    char *host;
+    char *port;
+    char colon[2] = ":";
+    char space[2] = " ";
+    for (int i = 0; i < nhosts; i++) {
+        host = strtok(strdup(hosts[i]), colon);
+        port = strtok(NULL, colon);
+        printf("\tQuerying cost from host %d %s\n", i, hosts[i]);
+
+        int sock = create_socket(host, port);
+        char *msg = "cost-query";
+
+        send_msg(sock, msg, strlen(msg));
+        printf("\t\t--> %s\n", (char *)msg);
+        int msgsize; 
+        char *recved_data = (char *) recv_msg(sock, &msgsize);
+        printf("\t\t<-- %s\n", recved_data);
+
+        if ( strcmp(strtok(recved_data, space), "cost") == 0 ) {
+            int cost = atoi(strtok(NULL, space));
+
+            if (cost < mincost) {
+                best_host_index = i;
+                mincost = cost;
+            }
+
+        } else {
+            fprintf(stderr, "Couldn't retrieve cost from %s:%s\n", host, port); 
+            exit(EXIT_FAILURE);
+        }
+
+    }
+    printf("\t\tSelecting %s\n", hosts[best_host_index]);
+    return best_host_index;
+}
+
+int send_command(int cmdindex, COMMAND *cmddata) {
     //printf("command index %d %s\n", i, cmddata->command);
+    char *hostname;
+    char *port;
+    char colon[2] = ":";
+    char space[2] = " ";
+    
+    if (startswith("remote-", cmddata->command)) {
+        char* host = strdup(hosts[find_host()]);
+        hostname = strtok(host, colon);
+        port = strtok(NULL, colon);
+    } else {
+        hostname = "localhost";
+        port = default_port;
+    }
+    printf("\tAttempting connection to %s:%s\n", hostname, port);
+    int sock = create_socket(hostname, port);
+    int n_required_files = 0;
+    if (cmddata->requires_files) {
+        n_required_files = strtokcount(cmddata->required_files, " ");
+    }
 
-    int sock = create_socket("localhost", "40000");
-    char *msg = "cost-query";
-    printf("\t--> %s\n", (char *)msg);
+    size_t buf_size =  snprintf(NULL, 0, "%d %d %s", cmdindex, n_required_files, cmddata->command);
 
+    char *msg = calloc(1,buf_size+1);
+    sprintf(msg, "%d %d %s", cmdindex, n_required_files, cmddata->command);
+    
     send_msg(sock, msg, strlen(msg));
-    char *cost = (char *) recv_msg(sock);
-    printf("\t<-- %s\n", cost);
-    //freeaddrinfo(res); // free the linked list
+    printf("\t\t--> %s\n", msg);
 
-    //if (startswith("remote-", cmddata->command)) {}*/
+    if (cmddata->requires_files) {
+        char *filename;
+        struct stat st;
+        size_t filesize;
+        //int fd;
+        void *msg;
+        int n;
+                
+        for (int i = 0; i < n_required_files; i++) {
+            if (i == 0) {
+                filename = strtok(cmddata->required_files, space);
+            } else {
+                filename = strtok(NULL, space);
+            }
+            send_msg(sock, filename, strlen(filename));
+            printf("\t\t--> %s\n", filename);
+            stat(filename, &st);
+            filesize = st.st_size;
+            printf("\t\tfilesize %ld\n", filesize);
+            msg = calloc(1, filesize+1);
+            //fd = open(filename, O_RDONLY);
+            //n = read(fd, msg, filesize);
+            FILE *fp = fopen(filename, "rb");
+            int nbytes = fread(msg, 1, filesize, fp);
+            printf("\t\tRead %d bytes", nbytes);
+            send_msg(sock, msg, filesize);
+            printf("\t\t--> file (size %d)\n", n);
+            //close(fd);
+            fclose(fp);
+        }
+
+    }
+
+    return sock;
+
 }
 
 void execute_actionsets() {
@@ -260,13 +359,86 @@ void execute_actionsets() {
         if (verbose) printf("------------Starting %s------------\n", actionsetname);
         cmdlist = actionsets[i].commands;
         ncmds = actionsets[i].ncommands;
+        int sockets[ncmds];
+        char *stdouts[ncmds];
+        char *stderrs[ncmds];
+        int exitcodes[ncmds];
 
         for (int j = 0; j < ncmds; j++) {
             if (verbose) printf("%s\n", cmdlist[j].command);
-            send_command(j, &cmdlist[j]);
+            sockets[j] = send_command(j, &cmdlist[j]);
+            printf("\tsocketfd %d\n", sockets[j]);
+        }
 
+        struct timeval tv = {1, 1000000};
+
+        fd_set read_fds;
+        int max_fd = 0;
+        int cmds_returned = 0;
+        int msgsize = 0;
+        while (true) {
+            FD_ZERO(&read_fds);
+            
+            for (int j = 0; j < ncmds; j++) {
+                FD_SET(sockets[j], &read_fds);
+                if (sockets[j] > max_fd) max_fd = sockets[j];
+            }
+            if (select(max_fd+1, &read_fds, NULL, NULL, &tv) == -1) {
+                perror("select() error\n");
+                exit(1);
+            }
+             
+            for (int j = 0; j < ncmds; j++) {
+                //printf("sockets[%d]=%d  %d\n", j ,sockets[j],FD_ISSET(sockets[j], &read_fds));
+                if (FD_ISSET(sockets[j], &read_fds)) {
+                    char *msg1 = recv_msg(sockets[j], &msgsize);
+                    if (msg1 == NULL) {
+                        //printf("socket %d closed\n", j);
+                        continue;
+                    }
+
+                    char *msg2 = recv_msg(sockets[j],  &msgsize);
+                    char *msg3 = recv_msg(sockets[j],  &msgsize);
+
+                    char space[2] = " ";
+
+                    int cmdindex = atoi(strtok(strdup(msg1), space));
+                    int exitcode = atoi(strtok(NULL, space));
+                    int nfiles = atoi(strtok(NULL, space));
+
+
+                    exitcodes[j] = exitcode;
+                    stdouts[j] = msg2;
+                    stderrs[j] = msg3;
+                    printf("\t<-- cmdindex exitcode nfile %d %d %d\n", cmdindex, exitcode, nfiles);
+                    printf("\t<-- stdout (length %ld)\n", strlen(msg2));
+                    printf("\t<-- stderr (length %ld)\n", strlen(msg3));
+                    cmds_returned++;
+
+                    if (nfiles > 0) {
+                        char *filename = recv_msg(sockets[j], &msgsize);
+                        msgsize = 0;
+                        void *filecontents = recv_msg(sockets[j], &msgsize);
+                        FILE *fp = fopen(filename, "wb");
+                        fwrite(filecontents, msgsize, 1,  fp);
+                        printf("\t<-- %s\n", filename);
+                        printf("\t<-- file (%d)\n", msgsize);
+                        printf("\tWrote file %s\n", filename);
+                        fclose(fp);
+                    }
+                }
+            }
+            if (cmds_returned >= ncmds) {
+                break;
+            }
 
         }
+        for (int j = 0; j < ncmds; j++) {
+            printf("Command %d exitcode: %d\n", j, exitcodes[j]);
+            printf("Command %d stdout: \n%s\n", j, stdouts[j]);
+            printf("Command %d stderr: \n%s\n", j, stderrs[j]);
+        }
+ 
     }
 }
 
--- rake-c/rake-c.h
+++ rake-c/rake-c.h
@@ -5,6 +5,10 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
